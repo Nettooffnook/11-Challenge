c1bc70e61aa1eeef068e390a051ce93167bacea6
Nettooffnook <129340125+Nettooffnook@users.noreply.github.com>
2023-08-02 19:57:39 -0400

    Add files via upload

---
+++ 11-Challenge/server.js
@@ -0,0 +1,25 @@
+// importing our requirements for this project
+// setting constants to require express,
+// our routes for the api and html 
+// and a connection 3001 for us to use
+const express = require('express');
+const api = require('./routes/apiRoutes.js');
+const html = require('./routes/htmlRoutes.js');
+const PORT = process.env.PORT || 3001;
+
+//express const for us to use
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', api);
+app.use('/', html);
+
+
+// listener for our app and returning message that app is 
+// listening at our port
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
